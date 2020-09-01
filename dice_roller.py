31be8b0ad3bb822c9b38676dd59e2291c798fb16
capblack222 <capblack222@github.com>
2020-09-02 02:18:15 +0530

    Now can roll 2 dice

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,13 @@
+import random as r
+roll=2
+
 def main():
-  print('You rolled a die')
+  roll_sum = 0
+  for i in range(0,roll):
+    n = r.randint(1, 6)
+    roll_sum=roll_sum+n
+    print(f'You have rolled a {n}')
+  print(f'You have rolled a total of {roll_sum}')
 
 if __name__== "__main__":
   main()
